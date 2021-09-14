fbddf8a5e917486f85c8ac041be04e65121a81ff
mbeniwal <mohit.beniwal@verizonmedia.com>
2021-09-14 15:33:40 +0530

    added screenshots

--- udagram-react-frontend/src/Config.js
+++ udagram-react-frontend/src/Config.js
@@ -1,4 +1,5 @@
-const { REACT_APP_BACKEND_HOST } = process.env;
+const REACT_APP_BACKEND_HOST =
+  "http://a2401a0f0d8b64df0855c9d4844f4c4e-1440663746.us-east-2.elb.amazonaws.com:8080";
 export const GET_FEED_API = `${REACT_APP_BACKEND_HOST}/api/v0/feed`;
 export const POST_FEED_API = `${REACT_APP_BACKEND_HOST}/api/v0/feed`;
 export const POST_VIDEO_API = `${REACT_APP_BACKEND_HOST}/api/v0/video`;
